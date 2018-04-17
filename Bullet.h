857b4dfb0ecce5c3b87f10ea1b504918a1a41b95
diego-maya <38476363+diego-maya@users.noreply.github.com>
2018-04-17 15:23:35 -0500

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -1,6 +1,6 @@
 /*
 Author:             Mark Fuller
-                    Diego Maya	//test 2
+                    Diego Maya
 Assignment:         Galaga : Bullet : Header File
 Date Created:       4/3/2018
 Date Last Modified: 4/3/2018 : (Mark)Initial Header variables and functions.
